7a70b0565bf77c8cf8e9698cc1005b17f9ff693f
social-ouch <2837786@gmail.com>
2025-05-08 22:16:15 +0900

    Update and rename callstack_2025350044_이준범.c to callstack_2025350044_이준범(3).c

--- callstack_2025350044_이준범(3).c
+++ callstack_2025350044_이준범(3).c
@@ -1,38 +1,38 @@
 /*  call_stack
 
-    ½ÇÁ¦ ½Ã½ºÅÛ¿¡¼­´Â ½ºÅÃÀÌ ¸Ş¸ğ¸®¿¡ ÀúÀåµÇÁö¸¸, º» °úÁ¦¿¡¼­´Â `int` ¹è¿­À» ÀÌ¿ëÇÏ¿© ¸Ş¸ğ¸®¸¦ ±¸ÇöÇÕ´Ï´Ù.
-    ¿ø·¡´Â SFP¿Í Return Address¿¡ ½ÇÁ¦ °¡»ó ¸Ş¸ğ¸® ÁÖ¼Ò°¡ µé¾î°¡°ÚÁö¸¸, ÀÌ¹ø °úÁ¦¿¡¼­´Â -1·Î ´ëÃ¼ÇÕ´Ï´Ù.
+    ì‹¤ì œ ì‹œìŠ¤í…œì—ì„œëŠ” ìŠ¤íƒì´ ë©”ëª¨ë¦¬ì— ì €ì¥ë˜ì§€ë§Œ, ë³¸ ê³¼ì œì—ì„œëŠ” `int` ë°°ì—´ì„ ì´ìš©í•˜ì—¬ ë©”ëª¨ë¦¬ë¥¼ êµ¬í˜„í•©ë‹ˆë‹¤.
+    ì›ë˜ëŠ” SFPì™€ Return Addressì— ì‹¤ì œ ê°€ìƒ ë©”ëª¨ë¦¬ ì£¼ì†Œê°€ ë“¤ì–´ê°€ê² ì§€ë§Œ, ì´ë²ˆ ê³¼ì œì—ì„œëŠ” -1ë¡œ ëŒ€ì²´í•©ë‹ˆë‹¤.
 
-    int call_stack[]      : ½ÇÁ¦ µ¥ÀÌÅÍ(`int °ª`) ¶Ç´Â `-1` (¸ŞÅ¸µ¥ÀÌÅÍ ±¸ºĞ¿ë)À» ÀúÀåÇÏ´Â int ¹è¿­
-    char stack_info[][]   : call_stack[]°ú °°Àº À§Ä¡(index)¿¡ ´ëÇÑ ¼³¸íÀ» ÀúÀåÇÏ´Â ¹®ÀÚ¿­ ¹è¿­
+    int call_stack[]      : ì‹¤ì œ ë°ì´í„°(`int ê°’`) ë˜ëŠ” `-1` (ë©”íƒ€ë°ì´í„° êµ¬ë¶„ìš©)ì„ ì €ì¥í•˜ëŠ” int ë°°ì—´
+    char stack_info[][]   : call_stack[]ê³¼ ê°™ì€ ìœ„ì¹˜(index)ì— ëŒ€í•œ ì„¤ëª…ì„ ì €ì¥í•˜ëŠ” ë¬¸ìì—´ ë°°ì—´
 
-    ==========================call_stack ÀúÀå ±ÔÄ¢==========================
-    ¸Å°³ º¯¼ö / Áö¿ª º¯¼ö¸¦ pushÇÒ °æ¿ì   : int °ª ±×´ë·Î
-    Saved Frame Pointer ¸¦ pushÇÒ °æ¿ì  : call_stack¿¡¼­ÀÇ index
-    ¹İÈ¯ ÁÖ¼Ò°ªÀ» pushÇÒ °æ¿ì       : -1
+    ==========================call_stack ì €ì¥ ê·œì¹™==========================
+    ë§¤ê°œ ë³€ìˆ˜ / ì§€ì—­ ë³€ìˆ˜ë¥¼ pushí•  ê²½ìš°   : int ê°’ ê·¸ëŒ€ë¡œ
+    Saved Frame Pointer ë¥¼ pushí•  ê²½ìš°  : call_stackì—ì„œì˜ index
+    ë°˜í™˜ ì£¼ì†Œê°’ì„ pushí•  ê²½ìš°       : -1
     =======================================================================
 
 
-    ==========================stack_info ÀúÀå ±ÔÄ¢==========================
-    ¸Å°³ º¯¼ö / Áö¿ª º¯¼ö¸¦ pushÇÒ °æ¿ì        : º¯¼ö¿¡ ´ëÇÑ ¼³¸í
-    Saved Frame Pointer ¸¦ pushÇÒ °æ¿ì  : ¾î¶² ÇÔ¼öÀÇ SFPÀÎÁö
-    ¹İÈ¯ ÁÖ¼Ò°ªÀ» pushÇÒ °æ¿ì                 : "Return Address"
+    ==========================stack_info ì €ì¥ ê·œì¹™==========================
+    ë§¤ê°œ ë³€ìˆ˜ / ì§€ì—­ ë³€ìˆ˜ë¥¼ pushí•  ê²½ìš°        : ë³€ìˆ˜ì— ëŒ€í•œ ì„¤ëª…
+    Saved Frame Pointer ë¥¼ pushí•  ê²½ìš°  : ì–´ë–¤ í•¨ìˆ˜ì˜ SFPì¸ì§€
+    ë°˜í™˜ ì£¼ì†Œê°’ì„ pushí•  ê²½ìš°                 : "Return Address"
     ========================================================================
 */
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define STACK_SIZE 50 // ÃÖ´ë ½ºÅÃ Å©±â
+#define STACK_SIZE 50 // ìµœëŒ€ ìŠ¤íƒ í¬ê¸°
 
-int     call_stack[STACK_SIZE];         // Call StackÀ» ÀúÀåÇÏ´Â ¹è¿­
-char    stack_info[STACK_SIZE][20];     // Call Stack ¿ä¼Ò¿¡ ´ëÇÑ ¼³¸íÀ» ÀúÀåÇÏ´Â ¹è¿­
+int     call_stack[STACK_SIZE];         // Call Stackì„ ì €ì¥í•˜ëŠ” ë°°ì—´
+char    stack_info[STACK_SIZE][20];     // Call Stack ìš”ì†Œì— ëŒ€í•œ ì„¤ëª…ì„ ì €ì¥í•˜ëŠ” ë°°ì—´
 
 /*  SP (Stack Pointer), FP (Frame Pointer)
 
-    SP´Â ÇöÀç ½ºÅÃÀÇ ÃÖ»ó´Ü ÀÎµ¦½º¸¦ °¡¸®Åµ´Ï´Ù.
-    ½ºÅÃÀÌ ºñ¾îÀÖÀ» ¶§ SP = -1, ÇÏ³ª°¡ ½×ÀÌ¸é `call_stack[0]` -> SP = 0, `call_stack[1]` -> SP = 1, ...
+    SPëŠ” í˜„ì¬ ìŠ¤íƒì˜ ìµœìƒë‹¨ ì¸ë±ìŠ¤ë¥¼ ê°€ë¦¬í‚µë‹ˆë‹¤.
+    ìŠ¤íƒì´ ë¹„ì–´ìˆì„ ë•Œ SP = -1, í•˜ë‚˜ê°€ ìŒ“ì´ë©´ `call_stack[0]` -> SP = 0, `call_stack[1]` -> SP = 1, ...
 
-    FP´Â ÇöÀç ÇÔ¼öÀÇ ½ºÅÃ ÇÁ·¹ÀÓ Æ÷ÀÎÅÍÀÔ´Ï´Ù.
-    ½ÇÇà ÁßÀÎ ÇÔ¼ö ½ºÅÃ ÇÁ·¹ÀÓÀÇ sfp¸¦ °¡¸®Åµ´Ï´Ù.
+    FPëŠ” í˜„ì¬ í•¨ìˆ˜ì˜ ìŠ¤íƒ í”„ë ˆì„ í¬ì¸í„°ì…ë‹ˆë‹¤.
+    ì‹¤í–‰ ì¤‘ì¸ í•¨ìˆ˜ ìŠ¤íƒ í”„ë ˆì„ì˜ sfpë¥¼ ê°€ë¦¬í‚µë‹ˆë‹¤.
 */
 int SP = -1;
 int FP = -1;
@@ -63,8 +63,8 @@ void pop(void) {
 }
 
 /*
-    ÇöÀç call_stack ÀüÃ¼¸¦ Ãâ·ÂÇÕ´Ï´Ù.
-    ÇØ´ç ÇÔ¼öÀÇ Ãâ·Â °á°úµéÀ» ¹ÙÅÁÀ¸·Î ±¸Çö ¿Ï¼ºµµ¸¦ Æò°¡ÇÒ ¿¹Á¤ÀÔ´Ï´Ù.
+    í˜„ì¬ call_stack ì „ì²´ë¥¼ ì¶œë ¥í•©ë‹ˆë‹¤.
+    í•´ë‹¹ í•¨ìˆ˜ì˜ ì¶œë ¥ ê²°ê³¼ë“¤ì„ ë°”íƒ•ìœ¼ë¡œ êµ¬í˜„ ì™„ì„±ë„ë¥¼ í‰ê°€í•  ì˜ˆì •ì…ë‹ˆë‹¤.
 */
 void print_stack()
 {
@@ -94,7 +94,7 @@ void print_stack()
 }
 
 
-//func ³»ºÎ´Â ÀÚÀ¯·Ó°Ô Ãß°¡ÇØµµ ±¦ÂúÀ¸³ª, ¾Æ·¡ÀÇ ±¸Á¶¸¦ ¹Ù²ÙÁö´Â ¸¶¼¼¿ä
+//func ë‚´ë¶€ëŠ” ììœ ë¡­ê²Œ ì¶”ê°€í•´ë„ ê´œì°®ìœ¼ë‚˜, ì•„ë˜ì˜ êµ¬ì¡°ë¥¼ ë°”ê¾¸ì§€ëŠ” ë§ˆì„¸ìš”
 void func1(int arg1, int arg2, int arg3)
 {
     if (home == 1)
@@ -117,21 +117,21 @@ void func1(int arg1, int arg2, int arg3)
 
     do
     {
-        printf(" ¹«¾ùÀ» ÇÒ±î?? \n");
+        printf(" ë¬´ì—‡ì„ í• ê¹Œ?? \n");
         printf("1 : pop     2 : push    3 : exit    4 : show stack \n");
         scanf(" %d", &res);
 
 
         if (res == 2)
         {
-            printf("\npush¸¦ ¼±ÅÃÇÏ¼Ì½À´Ï´Ù.\n\n");
+            printf("\npushë¥¼ ì„ íƒí•˜ì…¨ìŠµë‹ˆë‹¤.\n\n");
             home = 1;
             func2(11, 13);
         }
 
         else if (res == 1) 
         {
-            printf("\npopÀ» ¼±ÅÃÇÏ¼Ì½À´Ï´Ù.\n\n");
+            printf("\npopì„ ì„ íƒí•˜ì…¨ìŠµë‹ˆë‹¤.\n\n");
 
             pop();
             pop();
@@ -148,7 +148,7 @@ void func1(int arg1, int arg2, int arg3)
             print_stack();
         }
 
-        else if (res == 3) printf("Á¾·áÇÕ´Ï´Ù");
+        else if (res == 3) printf("ì¢…ë£Œí•©ë‹ˆë‹¤");
     
     } while (res == 4);
 }
@@ -175,20 +175,20 @@ void func2(int arg1, int arg2)
 
     do
     {
-        printf(" ¹«¾ùÀ» ÇÒ±î?? \n");
+        printf(" ë¬´ì—‡ì„ í• ê¹Œ?? \n");
         printf("1 : pop     2 : push    3 : exit    4 : show stack \n");
         scanf(" %d", &res);
 
 
         if (res == 2)
         {
-            printf("\npush¸¦ ¼±ÅÃÇÏ¼Ì½À´Ï´Ù.\n\n");
+            printf("\npushë¥¼ ì„ íƒí•˜ì…¨ìŠµë‹ˆë‹¤.\n\n");
             func3(77);
         }
 
         else if (res == 1)
         {
-            printf("\npopÀ» ¼±ÅÃÇÏ¼Ì½À´Ï´Ù.\n\n");
+            printf("\npopì„ ì„ íƒí•˜ì…¨ìŠµë‹ˆë‹¤.\n\n");
 
             pop();
             pop();
@@ -206,7 +206,7 @@ void func2(int arg1, int arg2)
             print_stack();
         }
 
-        else if (res == 3) printf("Á¾·áÇÕ´Ï´Ù");
+        else if (res == 3) printf("ì¢…ë£Œí•©ë‹ˆë‹¤");
 
     } while (res == 4);
 
@@ -231,21 +231,21 @@ void func3(int arg1)
 
     do
     {
-        printf(" ¹«¾ùÀ» ÇÒ±î?? \n");
+        printf(" ë¬´ì—‡ì„ í• ê¹Œ?? \n");
         printf("1 : pop     2 : push    3 : exit    4 : show stack \n");
         scanf(" %d", &res);
 
 
         if (res == 2)
         {
-            printf("\npush¸¦ ¼±ÅÃÇÏ¼Ì½À´Ï´Ù.\n\n");
+            printf("\npushë¥¼ ì„ íƒí•˜ì…¨ìŠµë‹ˆë‹¤.\n\n");
             printf("Stack is full !\n");
 
         }
 
         else if (res == 1)
         {
-            printf("\npopÀ» ¼±ÅÃÇÏ¼Ì½À´Ï´Ù.\n\n");
+            printf("\npopì„ ì„ íƒí•˜ì…¨ìŠµë‹ˆë‹¤.\n\n");
 
             pop();
             pop();
@@ -263,7 +263,7 @@ void func3(int arg1)
             print_stack();
         }
 
-        else if (res == 3) printf("Á¾·áÇÕ´Ï´Ù");
+        else if (res == 3) printf("ì¢…ë£Œí•©ë‹ˆë‹¤");
 
     } while (res == 4 | res == 2);
 
@@ -271,26 +271,26 @@ void func3(int arg1)
 }
 
 
-//main ÇÔ¼ö¿¡ °ü·ÃµÈ stack frameÀº ±¸ÇöÇÏÁö ¾Ê¾Æµµ µË´Ï´Ù.
+//main í•¨ìˆ˜ì— ê´€ë ¨ëœ stack frameì€ êµ¬í˜„í•˜ì§€ ì•Šì•„ë„ ë©ë‹ˆë‹¤.
 int main()
 {   
     int res;
     do {
         
-        printf("¹«¾ùÀ» ÇÒ±î? \n");
+        printf("ë¬´ì—‡ì„ í• ê¹Œ? \n");
         printf("1 : pop     2 : push    3 : exit    4 : show stack \n");
         scanf(" %d", &res);
 
         if (res == 2)
         {
-            printf("\npush¸¦ ¼±ÅÃÇÏ¼Ì½À´Ï´Ù.\n\n");
+            printf("\npushë¥¼ ì„ íƒí•˜ì…¨ìŠµë‹ˆë‹¤.\n\n");
             home = 1;
             func1(1, 2, 3);
         }
 
         else if (res == 1) 
         {
-            printf("\npopÀ» ¼±ÅÃÇÏ¼Ì½À´Ï´Ù.\n\n");
+            printf("\npopì„ ì„ íƒí•˜ì…¨ìŠµë‹ˆë‹¤.\n\n");
 
             printf("stack is empty!\n");
         }
@@ -300,7 +300,7 @@ int main()
             print_stack();
         }
 
-        else if (res == 3) printf("Á¾·áÇÕ´Ï´Ù");
+        else if (res == 3) printf("ì¢…ë£Œí•©ë‹ˆë‹¤");
 
     } while (res == 1 | res == 4);
     return 0;
